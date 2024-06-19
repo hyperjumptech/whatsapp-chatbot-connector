ab2f95b90ecd6400e25eb419d234e592ee37e29c
Raosan <raosanf.lillahi@gmail.com>
2024-06-19 13:39:19 +0700

    add test

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/node_modules/'
+  ],
+};
