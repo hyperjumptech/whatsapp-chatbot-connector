b66b1481f895f32396f8858f7d392e2df0b55724
Irwansyah <irwansyah@gmail.com>
2024-07-14 18:31:22 +0700

    chore: refactor app.ts

--- app.ts
+++ app.ts
@@ -2,17 +2,22 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-
-import webhookRoutes from "./api/webhook";
+import { getWebhookHandler } from "./services/webhook-handler-active-object";
+import cors  from 'cors';
 
 // Load environment variables from .env file
 dotenv.config();
 
+getWebhookHandler().init().catch(error => console.error(error));
+
 const { NODE_ENV } = process.env;
 
-const app = express();
+export const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors());
+
 
 if (NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -20,8 +25,6 @@ if (NODE_ENV === "development") {
   app.use(morgan("common"));
 }
 
-app.get("/", (_req, res) => res.send("Whatsapp Chatbot Connector by Hyperjump"));
 
-app.use("/webhook", webhookRoutes);
 
-module.exports = app;
+
