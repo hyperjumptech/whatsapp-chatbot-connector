26472886fa166e8dd5281d0a0ee3b9c799fd96cb
Irwansyah <irwansyah@gmail.com>
2024-07-17 16:55:46 +0700

    chore: refactor index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
-const app = require("./app");
+import {app} from './app';
+
+import "./api/webhook";
 
 const port = 5007;
 
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
+
+
+
