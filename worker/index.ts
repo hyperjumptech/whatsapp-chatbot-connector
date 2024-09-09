3041ef47fac27aa9fd3ecfdb66ac47b5d03dbc1a
Raosan <raosanf.lillahi@gmail.com>
2024-09-10 05:23:57 +0700

    auto removal worker

--- worker/index.ts
+++ worker/index.ts
@@ -2,6 +2,9 @@ import { Worker } from "bullmq";
 import { config } from "../utils/config";
 import { _markChatAsRead, _queryAndReply } from "../services/whatsapp";
 
+const hour = 3600;
+const day = hour * 24;
+
 const worker = new Worker(
   "messages",
   async (job) => {
@@ -17,6 +20,14 @@ const worker = new Worker(
       port: config.REDIS_PORT,
     },
     autorun: false,
+    removeOnComplete: {
+      age: 3 * day,
+      count: 1000,
+    },
+    removeOnFail: {
+      age: 3 * day,
+      count: 2000,
+    },
   }
 );
 
